df9d04c1c4883e5a73923cbc9f01d0fb53b7521b
Nimra-gul1 <nimraofficial121@gmail.com>
2024-10-20 21:20:29 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+let colors = ["pink", "grey", "purple", "green"];
+console.log("Original array:", colors);
+
+colors.pop(); 
+colors.shift(); //will remove first element
+
+console.log("Updated array:", colors);
